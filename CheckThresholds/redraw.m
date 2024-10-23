f45d00e94d711670c0520d948a24c95018363a9e
Hrishikesh Bhagwat <hbhagwat@umich.edu>
2024-10-23 11:24:42 -0400

    Adjust label position and size

--- CheckThresholds/redraw.m
+++ CheckThresholds/redraw.m
@@ -7,14 +7,14 @@ function redraw(frame, vidObj, idx, behavior)
 % Read frame
 f = vidObj.read(frame);
 
-f = insertText(f,[0 0], sprintf('Frame: %d', frame), 'FontSize', 20);
+f = insertText(f,[0 0], sprintf('Frame: %d', frame), 'FontSize', round(vidObj.Width/30));
 
 if strcmp(behavior,'freezing')
     if frame > 1
         if idx(frame-1)
-            I = insertText(f,[vidObj.Width-90 0], 'Freezing', 'TextBoxColor', 'red', 'FontSize', 20);
+            I = insertText(f,[vidObj.Width-(vidObj.Width/6) 0], 'Freezing', 'TextBoxColor', 'red', 'FontSize', round(vidObj.Width/30));
         else
-            I = insertText(f,[vidObj.Width-85 0], 'Moving', 'TextBoxColor', 'green', 'FontSize', 20);
+            I = insertText(f,[vidObj.Width-(vidObj.Width/6)+(vidObj.Width/60) 0], 'Moving', 'TextBoxColor', 'green', 'FontSize', round(vidObj.Width/30));
         end
     else
         I = f;
@@ -22,7 +22,7 @@ if strcmp(behavior,'freezing')
 elseif strcmp(behavior,'struggle')
     if frame > 1
         if idx(frame-1)
-            I = insertText(f,[vidObj.Width-110 0], 'Struggling', 'TextBoxColor', 'red', 'FontSize', 20);
+            I = insertText(f,[vidObj.Width-(vidObj.Width/6)-(vidObj.Width/40) 0], 'Struggling', 'TextBoxColor', 'red', 'FontSize', round(vidObj.Width/30));
         else
             I = f;
         end
